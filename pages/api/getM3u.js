caeab63defd16bf16ff16525a0c1249d73678fbd
krz123 <karizmazmr@ymail.com>
2024-07-22 16:37:43 +0530

    Update getM3u.js

--- pages/api/getM3u.js
+++ pages/api/getM3u.js
@@ -16,29 +16,29 @@ const getUserChanDetails = async () => {
     }
 
     try {
-        const responseChannels = await fetch("https://tplayapi.code-crafters.app/321codecrafters/fetcher.json");
+        const responseChannels = await fetch("https://toxicify-tpkeys.vercel.app/data/tplay.json");
         const cData = await responseChannels.json();
 
-        if (cData && cData.data && Array.isArray(cData.data.channels)) {
-            const flatChannels = cData.data.channels.flat();
+        if (cData && cData.data && Array.isArray(cData.data)) {
+            const flatChannels = cData.data.flat();
             flatChannels.forEach(channel => {
-                let firstGenre = channel.genres && channel.genres.length > 0 ? channel.genres[0] : null;
+                let firstGenre = channel.genre || null;
                 let rearrangedChannel = {
                     id: channel.id,
                     name: channel.name,
-                    tvg_id: channel.tvg_id,
+                    tvg_id: channel.id,
                     group_title: firstGenre,
-                    tvg_logo: channel.logo_url,
-                    stream_url: channel.manifest_url,
-                    license_url: channel.license_url,
-                    stream_headers: channel.manifest_headers ? (channel.manifest_headers['User-Agent'] || JSON.stringify(channel.manifest_headers)) : null,
-                    drm: channel.drm,
-                    is_mpd: channel.is_mpd,
-                    kid_in_mpd: channel.kid_in_mpd,
-                    hmac_required: channel.hmac_required,
-                    key_extracted: channel.key_extracted,
+                    tvg_logo: channel.logo,
+                    stream_url: channel.mpd,
+                    license_url: null,
+                    stream_headers: null,
+                    drm: null,
+                    is_mpd: true,
+                    kid_in_mpd: channel.kid,
+                    hmac_required: null,
+                    key_extracted: null,
                     pssh: channel.pssh,
-                    clearkey: channel.clearkeys ? JSON.stringify(channel.clearkeys[0].base64) : null,
+                    clearkey: channel.clearkeys_base64 ? JSON.stringify(channel.clearkeys_base64) : null,
                     hma: hmacValue
                 };
                 obj.list.push(rearrangedChannel);
@@ -60,15 +60,15 @@ const generateM3u = async (ud) => {
 
     m3uStr = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/mitthu786/tvepg/main/tataplay/epg.xml.gz"\n\n';
 
-  for (let i = 0; i < chansList.length; i++) {
-    m3uStr += '#EXTINF:-1 tvg-id="' + chansList[i].id.toString() + '" ';
-    m3uStr += 'group-title="' + (chansList[i].group_title) + '", tvg-logo="https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,h_250,w_250/' + (chansList[i].tvg_logo) + '", ' + chansList[i].name + '\n';
-    m3uStr += '#KODIPROP:inputstream.adaptive.license_type=clearkey\n';
-    m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + chansList[i].clearkey + '\n';
-    m3uStr += '#EXTVLCOPT:http-user-agent=' + chansList[i].stream_headers + '\n';
-    m3uStr += '#EXTHTTP:{"cookie":"' + chansList[i].hma + '"}\n';
-    m3uStr += chansList[i].stream_url + '|cookie:' + chansList[i].hma + '\n\n';
-}
+    for (let i = 0; i < chansList.length; i++) {
+        m3uStr += '#EXTINF:-1 tvg-id="' + chansList[i].id.toString() + '" ';
+        m3uStr += 'group-title="' + (chansList[i].group_title) + '", tvg-logo="' + chansList[i].tvg_logo + '", ' + chansList[i].name + '\n';
+        m3uStr += '#KODIPROP:inputstream.adaptive.license_type=clearkey\n';
+        m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + chansList[i].clearkey + '\n';
+        m3uStr += '#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36\n';
+        m3uStr += '#EXTHTTP:{"cookie":"' + chansList[i].hma + '"}\n';
+        m3uStr += chansList[i].stream_url + '|cookie:' + chansList[i].hma + '\n\n';
+    }
 
     console.log('all done!');
     return m3uStr;
