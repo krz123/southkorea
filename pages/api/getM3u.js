169a6223365ffe8d540335be10cb8361d383e8c2
krz123 <karizmazmr@ymail.com>
2024-08-01 15:03:06 +0530

    Update getM3u.js

--- pages/api/getM3u.js
+++ pages/api/getM3u.js
@@ -7,7 +7,7 @@ const getUserChanDetails = async () => {
     let obj = { list: [] };
 
     try {
-        const responseHmac = await fetch("https://tm.tapi.videoready.tv/portal-search/pub/api/v1/channels?limit=1000");
+        const responseHmac = await fetch("https://fox.toxic-gang.xyz/tata/hmac");
         const data = await responseHmac.json();
         hmacValue = data.data.hmac.hdntl.value;
     } catch (error) {
