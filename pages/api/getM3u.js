d580465bcdb058429458c3e99596a7e663cbaad7
krz123 <karizmazmr@ymail.com>
2024-08-03 00:06:12 +0530

    Update getM3u.js

--- pages/api/getM3u.js
+++ pages/api/getM3u.js
@@ -1,1 +1,98 @@
-Ly8gTmV4dC5qcyBBUEkgcm91dGUgc3VwcG9ydDogaHR0cHM6Ly9uZXh0anMub3JnL2RvY3MvYXBpLXJvdXRlcy9pbnRyb2R1Y3Rpb24KCmltcG9ydCBmZXRjaCBmcm9tICJjcm9zcy1mZXRjaCI7Cgpjb25zdCBnZXRVc2VyQ2hhbkRldGFpbHMgPSBhc3luYyAoKSA9PiB7CiAgICBsZXQgaG1hY1ZhbHVlOwogICAgbGV0IG9iaiA9IHsgbGlzdDogW10gfTsKCiAgICB0cnkgewogICAgICAgIGNvbnN0IHJlc3BvbnNlSG1hYyA9IGF3YWl0IGZldGNoKCJodHRwczovL2ZveC50b3hpYy1nYW5nLnh5ei90YXRhL2htYWMiKTsKICAgICAgICBjb25zdCBkYXRhID0gYXdhaXQgcmVzcG9uc2VIbWFjLmpzb24oKTsKICAgICAgICBjb25zdCBobWFjRGF0YSA9IGRhdGFbMF07CiAgICAgICAgaG1hY1ZhbHVlID0gaG1hY0RhdGEuZGF0YS5oZG50bDsKICAgIH0gY2F0Y2ggKGVycm9yKSB7CiAgICAgICAgY29uc29sZS5lcnJvcignRXJyb3IgZmV0Y2hpbmcgYW5kIHJlYXJyYW5naW5nIEhNQUMgZGF0YTonLCBlcnJvcik7CiAgICAgICAgcmV0dXJuIG9iajsKICAgIH0KCiAgICB0cnkgewogICAgY29uc3QgcmVzcG9uc2VDaGFubmVscyA9IGF3YWl0IGZldGNoKCJodHRwczovL2ZveC50b3hpYy1nYW5nLnh5ei90YXRhL2NoYW5uZWxzIik7CiAgICBjb25zdCBjRGF0YSA9IGF3YWl0IHJlc3BvbnNlQ2hhbm5lbHMuanNvbigpOwoKICAgIGlmIChjRGF0YSAmJiBjRGF0YS5kYXRhICYmIEFycmF5LmlzQXJyYXkoY0RhdGEuZGF0YSkpIHsKICAgICAgICBjb25zdCBmbGF0Q2hhbm5lbHMgPSBjRGF0YS5kYXRhLmZsYXQoKTsKICAgICAgICBmbGF0Q2hhbm5lbHMuZm9yRWFjaChjaGFubmVsID0+IHsKICAgICAgICAgICAgbGV0IGNsZWFya2V5VmFsdWUgPSBudWxsOwogICAgICAgICAgICBpZiAoY2hhbm5lbC5jbGVhcmtleXNfYmFzZTY0ICYmIHR5cGVvZiBjaGFubmVsLmNsZWFya2V5c19iYXNlNjQgPT09ICdvYmplY3QnKSB7CiAgICAgICAgICAgICAgICBjbGVhcmtleVZhbHVlID0gSlNPTi5zdHJpbmdpZnkoY2hhbm5lbC5jbGVhcmtleXNfYmFzZTY0KTsKICAgICAgICAgICAgfSBlbHNlIGlmIChjaGFubmVsLmNsZWFya2V5c19iYXNlNjQgJiYgdHlwZW9mIGNoYW5uZWwuY2xlYXJrZXlzX2Jhc2U2NCA9PT0gJ3N0cmluZycgJiYgY2hhbm5lbC5jbGVhcmtleXNfYmFzZTY0ICE9PSAnbnVsbCcpIHsKICAgICAgICAgICAgICAgIGNsZWFya2V5VmFsdWUgPSBjaGFubmVsLmNsZWFya2V5c19iYXNlNjQ7CiAgICAgICAgICAgIH0gZWxzZSBpZiAoY2hhbm5lbC5saWNlbmNlMSAmJiBjaGFubmVsLmxpY2VuY2UyKSB7CiAgICAgICAgICAgICAgICBjbGVhcmtleVZhbHVlID0gYCR7Y2hhbm5lbC5saWNlbmNlMX06JHtjaGFubmVsLmxpY2VuY2UyfWA7CiAgICAgICAgICAgIH0KCiAgICAgICAgICAgIGxldCBmaXJzdEdlbnJlID0gY2hhbm5lbC5nZW5yZXMgJiYgY2hhbm5lbC5nZW5yZXMubGVuZ3RoID4gMCA/IGNoYW5uZWwuZ2VucmVzWzBdIDogbnVsbDsKICAgICAgICAgICAgbGV0IHJlYXJyYW5nZWRDaGFubmVsID0gewogICAgICAgICAgICAgICAgaWQ6IGNoYW5uZWwuaWQsCiAgICAgICAgICAgICAgICBuYW1lOiBjaGFubmVsLnRpdGxlLAogICAgICAgICAgICAgICAgdHZnX2lkOiBjaGFubmVsLmlkLAogICAgICAgICAgICAgICAgZ3JvdXBfdGl0bGU6IGNoYW5uZWwuZ2VucmUsCiAgICAgICAgICAgICAgICB0dmdfbG9nbzogY2hhbm5lbC5sb2dvLAogICAgICAgICAgICAgICAgc3RyZWFtX3VybDogY2hhbm5lbC5pbml0aWFsVXJsLAogICAgICAgICAgICAgICAgbGljZW5zZV91cmw6IGNoYW5uZWwubGljZW5zZV91cmwsCiAgICAgICAgICAgICAgICBzdHJlYW1faGVhZGVyczogY2hhbm5lbC5tYW5pZmVzdF9oZWFkZXJzID8gKGNoYW5uZWwubWFuaWZlc3RfaGVhZGVyc1snVXNlci1BZ2VudCddIHx8IEpTT04uc3RyaW5naWZ5KGNoYW5uZWwubWFuaWZlc3RfaGVhZGVycykpIDogbnVsbCwKICAgICAgICAgICAgICAgIGRybTogY2hhbm5lbC5kcm0sCiAgICAgICAgICAgICAgICBpc19tcGQ6IGNoYW5uZWwuaXNfbXBkLAogICAgICAgICAgICAgICAga2lkX2luX21wZDogY2hhbm5lbC5raWRfaW5fbXBkLAogICAgICAgICAgICAgICAgaG1hY19yZXF1aXJlZDogY2hhbm5lbC5obWFjX3JlcXVpcmVkLAogICAgICAgICAgICAgICAga2V5X2V4dHJhY3RlZDogY2hhbm5lbC5rZXlfZXh0cmFjdGVkLAogICAgICAgICAgICAgICAgcHNzaDogY2hhbm5lbC5wc3NoLAogICAgICAgICAgICAgICAgY2xlYXJrZXk6IGNsZWFya2V5VmFsdWUsCiAgICAgICAgICAgICAgICBobWE6IGhtYWNWYWx1ZQogICAgICAgICAgICB9OwogICAgICAgICAgICBvYmoubGlzdC5wdXNoKHJlYXJyYW5nZWRDaGFubmVsKTsKICAgICAgICB9KTsKICAgIH0KfSBjYXRjaCAoZXJyb3IpIHsKICAgIGNvbnNvbGUuZXJyb3IoJ0ZldGNoIGVycm9yOicsIGVycm9yKTsKICAgIHJldHVybiBvYmo7Cn0KCgoKICAgIHJldHVybiBvYmo7Cn07Cgpjb25zdCBnZW5lcmF0ZU0zdSA9IGFzeW5jICh1ZCkgPT4gewogICAgbGV0IG0zdVN0ciA9ICcnOwoKICAgIGxldCB1c2VyQ2hhbkRldGFpbHMgPSBhd2FpdCBnZXRVc2VyQ2hhbkRldGFpbHMoKTsKICAgIGxldCBjaGFuc0xpc3QgPSB1c2VyQ2hhbkRldGFpbHMubGlzdDsKCiAgICBtM3VTdHIgPSAnI0VYVE0zVSB4LXR2Zy11cmw9Imh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9taXR0aHU3ODYvdHZlcGcvbWFpbi90YXRhcGxheS9lcGcueG1sLmd6IlxuXG4nOwoKICAgIGZvciAobGV0IGkgPSAwOyBpIDwgY2hhbnNMaXN0Lmxlbmd0aDsgaSsrKSB7CiAgICAgICAgbTN1U3RyICs9ICcjRVhUSU5GOi0xIHR2Zy1pZD0iJyArIGNoYW5zTGlzdFtpXS5pZC50b1N0cmluZygpICsgJyIgJzsKICAgICAgICBtM3VTdHIgKz0gJ2dyb3VwLXRpdGxlPSInICsgKGNoYW5zTGlzdFtpXS5ncm91cF90aXRsZSkgKyAnIiwgdHZnLWxvZ289Imh0dHBzOi8vbWVkaWFyZWFkeS52aWRlb3JlYWR5LnR2L3RhdGFza3ktZXBnL2ltYWdlL2ZldGNoL2ZfYXV0byxmbF9sb3NzeSxxX2F1dG8saF8yNTAsd18yNTAvJyArIChjaGFuc0xpc3RbaV0udHZnX2xvZ28pICsgJyIsICcgKyBjaGFuc0xpc3RbaV0ubmFtZSArICdcbic7CiAgICAgICAgbTN1U3RyICs9ICcjS09ESVBST1A6aW5wdXRzdHJlYW0uYWRhcHRpdmUubGljZW5zZV90eXBlPWNsZWFya2V5XG4nOwogICAgICAgIG0zdVN0ciArPSAnI0tPRElQUk9QOmlucHV0c3RyZWFtLmFkYXB0aXZlLmxpY2Vuc2Vfa2V5PScgKyBjaGFuc0xpc3RbaV0uY2xlYXJrZXkgKyAnXG4nOwogICAgICAgIG0zdVN0ciArPSAnI0VYVFZMQ09QVDpodHRwLXVzZXItYWdlbnQ9TW96aWxsYS81LjBcbic7CiAgICAgICAgbTN1U3RyICs9ICcjRVhUSFRUUDp7ImNvb2tpZSI6IicgKyBjaGFuc0xpc3RbaV0uaG1hICsgJyJ9XG4nOwogICAgICAgIG0zdVN0ciArPSBjaGFuc0xpc3RbaV0uc3RyZWFtX3VybCArICd8Y29va2llOicgKyBjaGFuc0xpc3RbaV0uaG1hICsgJ1xuXG4nOwogICAgfQoKICAgIGNvbnNvbGUubG9nKCdhbGwgZG9uZSEnKTsKICAgIHJldHVybiBtM3VTdHI7Cn07CgpleHBvcnQgZGVmYXVsdCBhc3luYyBmdW5jdGlvbiBoYW5kbGVyKHJlcSwgcmVzKSB7CiAgICBsZXQgdURhdGEgPSB7CiAgICAgICAgdHNBY3RpdmU6IHRydWUKICAgIH07CgogICAgaWYgKHVEYXRhLnRzQWN0aXZlKSB7CiAgICAgICAgbGV0IG0zdVN0cmluZyA9IGF3YWl0IGdlbmVyYXRlTTN1KHVEYXRhKTsKICAgICAgICByZXMuc3RhdHVzKDIwMCkuc2VuZChtM3VTdHJpbmcpOwogICAgfQp9
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import fetch from "cross-fetch";
+
+const getUserChanDetails = async () => {
+    let hmacValue;
+    let obj = { list: [] };
+
+    try {
+        const responseHmac = await fetch("https://fox.toxic-gang.xyz/tata/hmac");
+        const data = await responseHmac.json();
+        const hmacData = data[0];
+        hmacValue = hmacData.data.hdntl;
+    } catch (error) {
+        console.error('Error fetching and rearranging HMAC data:', error);
+        return obj;
+    }
+
+    try {
+    const responseChannels = await fetch("https://fox.toxic-gang.xyz/tata/channels");
+    const cData = await responseChannels.json();
+
+    if (cData && cData.data && Array.isArray(cData.data)) {
+        const flatChannels = cData.data.flat();
+        flatChannels.forEach(channel => {
+            let clearkeyValue = null;
+            if (channel.clearkeys_base64 && typeof channel.clearkeys_base64 === 'object') {
+                clearkeyValue = JSON.stringify(channel.clearkeys_base64);
+            } else if (channel.clearkeys_base64 && typeof channel.clearkeys_base64 === 'string' && channel.clearkeys_base64 !== 'null') {
+                clearkeyValue = channel.clearkeys_base64;
+            } else if (channel.licence1 && channel.licence2) {
+                clearkeyValue = `${channel.licence1}:${channel.licence2}`;
+            }
+
+            let firstGenre = channel.genres && channel.genres.length > 0 ? channel.genres[0] : null;
+            let rearrangedChannel = {
+                id: channel.id,
+                name: channel.title,
+                tvg_id: channel.id,
+                group_title: channel.genre,
+                tvg_logo: channel.logo,
+                stream_url: channel.initialUrl,
+                license_url: channel.license_url,
+                stream_headers: channel.manifest_headers ? (channel.manifest_headers['User-Agent'] || JSON.stringify(channel.manifest_headers)) : null,
+                drm: channel.drm,
+                is_mpd: channel.is_mpd,
+                kid_in_mpd: channel.kid_in_mpd,
+                hmac_required: channel.hmac_required,
+                key_extracted: channel.key_extracted,
+                pssh: channel.pssh,
+                clearkey: clearkeyValue,
+                hma: hmacValue
+            };
+            obj.list.push(rearrangedChannel);
+        });
+    }
+} catch (error) {
+    console.error('Fetch error:', error);
+    return obj;
+}
+
+
+
+    return obj;
+};
+
+const generateM3u = async (ud) => {
+    let m3uStr = '';
+
+    let userChanDetails = await getUserChanDetails();
+    let chansList = userChanDetails.list;
+
+    m3uStr = '#EXTM3U x-tvg-url="https://raw.githubusercontent.com/mitthu786/tvepg/main/tataplay/epg.xml.gz"\n\n';
+
+    for (let i = 0; i < chansList.length; i++) {
+        m3uStr += '#EXTINF:-1 tvg-id="' + chansList[i].id.toString() + '" ';
+        m3uStr += 'group-title="' + (chansList[i].group_title) + '", tvg-logo="https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,h_250,w_250/' + (chansList[i].tvg_logo) + '", ' + chansList[i].name + '\n';
+        m3uStr += '#KODIPROP:inputstream.adaptive.license_type=clearkey\n';
+        m3uStr += '#KODIPROP:inputstream.adaptive.license_key=' + chansList[i].clearkey + '\n';
+        m3uStr += '#EXTVLCOPT:http-user-agent=Mozilla/5.0\n';
+        m3uStr += '#EXTHTTP:{"cookie":"' + chansList[i].hma + '"}\n';
+        m3uStr += chansList[i].stream_url + '|cookie:' + chansList[i].hma + '\n\n';
+    }
+
+    console.log('all done!');
+    return m3uStr;
+};
+
+export default async function handler(req, res) {
+    let uData = {
+        tsActive: true
+    };
+
+    if (uData.tsActive) {
+        let m3uString = await generateM3u(uData);
+        res.status(200).send(m3uString);
+    }
+}
